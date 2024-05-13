904b273361816592db6ea442172b163d8ea6e517
Wiper-R <rshivang12345@gmail.com>
2024-05-13 11:23:15 +0530

    refactor: project restructure

---
+++ common/confetti/confetti.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+const CONFETTI = preload("res://common/confetti/confetti.tscn")
+
+const COLORS = [
+    '6cd9f1', # Light Blue
+    'a4cc42', # Lime Green
+    'f7ec8a', # Pale Yellow
+    'f18b72', # Salmon Pink
+    'f89cc0', # Light Pink
+    'f2bc7e'  # Peach
+];
+
+signal confetti;
+
+# Called from parent
+func _confetti():
+    for confetti in get_children() as Array[CPUParticles2D]:
+        confetti.emitting = true
+    
+
+func _ready() -> void:
+    confetti.connect(_confetti)
+    for color in COLORS:
+        var object = CONFETTI.instantiate()
+        object.color = Color(str(color))
+        add_child(object)
+
